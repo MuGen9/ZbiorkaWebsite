01d71367da09c1451c302df0326dd4035d5ce244
MuGen9 <maksparadowski@gmail.com>
2020-12-19 13:42:36 +0100

    remove react-scroll, add react-router-hashlink

--- src/components/Home/HomeHeader.js
+++ src/components/Home/HomeHeader.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { Link as LinkScroll, animateScroll as scroll } from 'react-scroll';
 import decoration from './../../assets/Decoration.svg';
+import { HashLink } from 'react-router-hash-link';
  
 function HomeHeader() {
     return (
@@ -11,12 +11,12 @@ function HomeHeader() {
                 <Link to="/rejestracja">Załóż konto</Link>
             </div>
             <div className="home__header__navigation">
-                <LinkScroll spy={true} smooth={true} duration={500} to='section1'>Start</LinkScroll>
-                <LinkScroll to='section2'>O co chodzi?</LinkScroll>
-                <LinkScroll to='section3'>O nas</LinkScroll>
-                <LinkScroll spy={true} smooth={true} duration={500} to='section1'>Fundacja i organizacje</LinkScroll>
-                <LinkScroll spy={true} smooth={true} duration={500} to='section1'>Kontakt</LinkScroll>
-                <Link to='/#section1'>Tresc</Link>
+                <HashLink to="/#section1">Start</HashLink>
+                <HashLink to="/#section1">O co chodzi?</HashLink>
+                <HashLink to="/#section1">O nas</HashLink>
+                <HashLink to="/#section1">Fundacja i organizacje</HashLink>
+                <HashLink to="/#section1">Kontakt</HashLink>
+                <HashLink to="/#section1">Treść</HashLink>
             </div>
             <div className="home__header__main">
                 <h2>Zacznij pomagać!</h2>
