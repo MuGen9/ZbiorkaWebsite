228a765819fbe8f361c72b2ee346336af2b06253
MuGen9 <maksparadowski@gmail.com>
2020-12-30 14:29:32 +0100

    login validation done

--- src/components/LogIn.js
+++ src/components/LogIn.js
@@ -1,9 +1,69 @@
-import React from 'react';
+import React, { useState } from 'react';
 import HomeHeaderTop from './Home/HomeHeaderTop';
 import decoration from './../assets/Decoration.svg';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 
 function LogIn() {
+    const [form, setForm] = useState({ email: "", password: "" });
+    const [errors, setErrors] = useState({ emailError: "", passwordError: "" });
+
+    const history = useHistory();
+
+    const handleChange = e => {
+        const {name, value} = e.target;
+        setForm(prevState => {
+            return {
+              ...prevState,
+              [name]: value
+            }
+        });
+    }
+
+    const handleSubmit = e => {
+        e.preventDefault();
+
+        if(validateEmail() && validatePass()) {
+            console.log('Zalogowano!');
+            history.push("/");
+        } else {
+            console.log('Popraw błędy');
+        }
+    }
+
+    const validateEmail = () => {
+        const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        let valid = re.test(String(form.email).toLowerCase());
+
+        setErrors(prevState => ({
+            ...prevState,
+            emailError: valid ? "" : "Nieprawidłowy email"
+        }));
+
+        return valid;
+    }
+
+    const validatePass = () => {
+        let valid = form.password.length > 5
+        
+        setErrors(prevState => ({
+            ...prevState,
+            passwordError: valid ? "" : "Przynajmniej 6 znaków"
+        }));
+
+        return valid;       
+    }
+
+    const handleLogIn = (e) => {
+        e.preventDefault();
+
+        if(validateEmail() && validatePass()) {
+            console.log('Zalogowano!')
+            history.push("/");
+        } else {
+            console.log('Popraw błędy')
+        }
+    }
+
     return (
         <div className="login">
             <HomeHeaderTop />
@@ -11,21 +71,23 @@ function LogIn() {
                 <h3>Logowanie</h3>
                 <img className="login__decoration" src={decoration} alt="Decoration" />
                 <div className="login__form">
-                    <form>
+                    <form onSubmit={handleSubmit}>
                         <label htmlFor="email">
-                            Email <br />
-                            <input type="text" name="email" />
+                            Email: <br />
+                            <input type="text" name="email" onChange={handleChange} onBlur={validateEmail} value={form.email} /><br/>
+                            <span>{errors.emailError}</span>
                         </label>
                         <br />
                         <label htmlFor="password">
-                            Hasło <br />
-                            <input type="password" name="password" />
+                            Hasło: <br />
+                            <input type="password" name="password" onChange={handleChange} onBlur={validatePass} value={form.password} /><br/>
+                            <span>{errors.passwordError}</span>
                         </label>
                     </form>
                 </div>
                 <div className="login__buttons">
                     <Link to="/rejestracja">Załóż konto</Link>
-                    <Link to="/">Zaloguj</Link>
+                    <Link to="/" onClick={handleLogIn}>Zaloguj</Link>
                 </div>
             </div>
         </div>
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import HomeHeaderTop from './Home/HomeHeaderTop';
 import decoration from './../assets/Decoration.svg';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 
 function Register() {
+    const [form, setForm] = useState({ email: "", password: "" });
+    const [errors, setErrors] = useState({ emailError: "", passwordError: "" });
+
+    const history = useHistory();
+
+    const handleChange = e => {
+        const {name, value} = e.target;
+        setForm(prevState => {
+            return {
+              ...prevState,
+              [name]: value
+            }
+        });
+    }
+
     return (
         <div className="register">
             <HomeHeaderTop />
@@ -13,17 +28,17 @@ function Register() {
                 <div className="register__form">
                     <form>
                         <label htmlFor="email">
-                            Email <br />
+                            Email: <br />
                             <input type="text" name="email" />
                         </label>
                         <br />
                         <label htmlFor="password">
-                            Hasło <br />
+                            Hasło: <br />
                             <input type="password" name="password" />
                         </label>
                         <br />
                         <label htmlFor="password2">
-                            Powtórz hasło <br />
+                            Powtórz hasło: <br />
                             <input type="password" name="password2" />
                         </label>
                     </form>
