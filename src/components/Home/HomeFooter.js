9bcb4b7dfefc17d3c929fc0e682c98fdf30054b3
MuGen9 <maksparadowski@gmail.com>
2020-12-30 15:19:52 +0100

    footer icons, responsivness fixes

--- src/components/Home/HomeFooter.js
+++ src/components/Home/HomeFooter.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import facebook from './../../assets/Facebook.svg';
+import instagram from './../../assets/Instagram.svg';
 
 function HomeFooter() {
     return (
         <div className="home__footer">
             <p>Copyright by Coders Lab</p>
+            <div className="home__footer__icons">
+                <a href="https://www.facebook.com/">
+                    <img src={facebook} alt="Facebook" />
+                </a>
+                <a href="https://www.instagram.com/">
+                    <img src={instagram} alt="Instagram" />
+                </a>
+            </div>
+            
         </div>
     )
 }
